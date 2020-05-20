ce39977299ec768536e69add0f191dee5e1e7a9d
otegami <a.s.takuya1026@gmail.com>
2020-05-20 11:15:19 +0900

    [Progressing] Hamming

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,11 +1,10 @@
 module Hamming
-  def self.compute(strand1, strand2)
-    if strand1.length == strand2.length
-      strand1.each_char.with_index.count do |nucleotide, index|
-        nucleotide != strand2[index]
-      end
-    else
-      raise ArgumentError
+  def self.compute(strand1, strand2) 
+    raise ArgumentError unless strand1.length == strand2.length
+
+    nucleotides = strand1.chars.zip(strand2.chars)
+    nucleotides.each.count do |nucleotide1, nucleotide2|
+      nucleotide1 != nucleotide2
     end
   end
 end
