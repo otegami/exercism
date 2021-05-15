70419f5a190018b41a9f8d783254aa343ae3d5c4
otegami <a.s.takuya1026@gmail.com>
2021-05-15 07:38:08 -0700

    refactoring hamming practice

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,10 +1,14 @@
 module Hamming
-  def self.compute(strand1, strand2) 
-    raise ArgumentError unless strand1.length == strand2.length
+  def self.compute(strand1, strand2)
+    raise StrandLengthError unless strand1.length == strand2.length
 
     nucleotides = strand1.chars.zip(strand2.chars)
-    nucleotides.each.count do |nucleotide1, nucleotide2|
-      nucleotide1 != nucleotide2
+    nucleotides.count { |nt1, nt2| nt1 != nt2 }
+  end
+
+  class StrandLengthError < ArgumentError
+    def initialize(message: 'strand lengths must be identical')
+      super(message)
     end
   end
 end
